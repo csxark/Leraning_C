7b8d7bd7b21101b95666403c499b38977e3cc74f
csxark <arktandoncs@gmail.com>
2024-07-13 12:35:15 +0530

    initial commit

--- 05_practiceset.c
+++ 05_practiceset.c
@@ -1,3 +1,26 @@
 #include<stdio.h>
 //Question 1
 //Write a program using function to find average of three numbers.
+/*int avg(int a, int b, int c){
+    float res=(a+b+c)/3;
+    printf("The average is:- %.2f",res);
+}
+int main(int argc, char const *argv[])
+{
+    avg(12,8,4);
+    return 0;
+}*/
+//Question 2
+/*Write a function to calculate force of attraction on a body of mass ‘m’ exerted by
+earth. Consider g = 9.8m/s2*/
+/*float force(int m){
+    return m*9.8;
+}
+int main(int argc, char const *argv[])
+{   
+    int m=90;
+    printf("the force of attraction is:- %.2f",force(m));
+    return 0;
+}*/
+//Question 3
+//Write a program using recursion to calculate nth element of Fibonacci series.
