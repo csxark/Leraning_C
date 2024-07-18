872089e62e9cc86c03f2352e60f1889fadfc303a
csxark <arktandoncs@gmail.com>
2024-07-18 21:19:55 +0530

    Create 00_practiceset.c

---
+++ 00_practiceset.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+
+//WAP to find all the even numbers between the range by the number
+/*int main(int argc, char const *argv[])
+{
+    int a,b;
+    printf("Enter the number:-");
+    scanf("%d",&a);
+    printf("Enter the number:-");
+    scanf("%d",&b);
+    for (int i = a; i <=b; i++)
+    {
+        if (i%2==0)
+        {
+            printf("%d, ",i);
+        }
+        
+    }
+}*/
+
+//WAP to find the factors of the number
+/*int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter a number:-");
+    scanf("%d",&n);
+    for (int i = 1; i <=n; i++)
+    {
+        if (n%i==0)
+        {
+            printf("%d\t",i);
+        }
+    }
+    return 0;
+}*/
+// wap to print tables of a range
+/*int main(int argc, char const *argv[])
+{
+    int a,b;
+    printf("Enter the number:-");
+    scanf("%d",&a);
+    printf("Enter the number:-");
+    scanf("%d",&b);
+    for (int i = a; i <=b; i++)
+    {   
+        for (int j = 1; j <11; j++)
+        {
+            printf("%d X %d = %d\n",i,j,i*j);
+        }    
+    }
+    
+    return 0;
+}*/
+
+//wap to find factorial 
+/*int main(int argc, char const *argv[])
+{
+    int num,c=1;
+    printf("Enter a number:-");
+    scanf("%d",&num);
+    for (int i = 1; i <=num; i++)
+    {
+        c*=i;
+    }
+    printf("The factorial is %d",c);
+    return 0;
+}*/
