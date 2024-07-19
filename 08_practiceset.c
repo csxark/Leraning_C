8fc6d0c54271cb0a66138e77a7548e1005414fad
csxark <arktandoncs@gmail.com>
2024-07-20 01:43:35 +0530

    Update 08_practiceset.c

--- 08_practiceset.c
+++ 08_practiceset.c
@@ -1,30 +1,51 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 // Question 1
 /*Write a function slice() to slice a string. It should change the original string such
 that it is now the sliced string. Take ‘m’ and ‘n’ as the start and ending position
 for slice.*/
-int slice(char s[], int m, int n)
+/*char* slice(char s[], int m, int n)
 {
-    /*char c[n-m + 1];
-    for (int i = m; i < n + 1; i++)
-    {   
-        c[i-m]=s[i];
-        --m;
-    }
-    printf("The sliced string is",c);*/
-
-    char *ptr1=&s[m];
-    char *ptr2=&s[n];
+    char (*ptr1)=&s[m];
+    char (*ptr2)=&s[n];
     s=ptr1;
-    s[n]='\0';
+    s[n-m+1]='\0';
     return s;
-
 }
+
 int main(int argc, char const *argv[])
 {
-    char st[]="thisisgoodnight";
-    int m=3,n=1;
+    char st[]="Harry Bhai";
+    int m=1,n=7;
     printf ("%s",slice(st,m,n));
+
+}*/
+// Question 2
+/*. Write a program to take string as an input from the user using %c and %s confirm
+that the strings are equal.*/
+/*int main(int argc, char const *argv[])
+{
+    char str[]="goodnight";
+    printf("This is a %c and %s",str[7],str);
+    return 0;
+}*/
+// Question 3
+// Write your own version of strlen function from <string.h>
+int sl(char str)
+{
+    int i = 0, count;
+    char s = str[i];
+    while (s != '/0')
+    {
+        s = str[i];
+        i++;
+    }    
+    return i - 1;
+}
+
+int main()
+{
+    char st[] = "goodnight";
+    printf("%d", sl(st));
     return 0;
 }
