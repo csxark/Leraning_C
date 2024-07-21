57d7db39d190c8ab82593ea6c9cdbcb31585855f
csxark <arktandoncs@gmail.com>
2024-07-21 11:51:16 +0530

    Update 00_practiceset.c

--- 00_practiceset.c
+++ 00_practiceset.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
 
-//WAP to find all the even numbers between the range by the number
+// WAP to find all the even numbers between the range by the number
 /*int main(int argc, char const *argv[])
 {
     int a,b;
@@ -14,11 +14,11 @@
         {
             printf("%d, ",i);
         }
-        
+
     }
 }*/
 
-//WAP to find the factors of the number
+// WAP to find the factors of the number
 /*int main(int argc, char const *argv[])
 {
     int n;
@@ -42,17 +42,17 @@
     printf("Enter the number:-");
     scanf("%d",&b);
     for (int i = a; i <=b; i++)
-    {   
+    {
         for (int j = 1; j <11; j++)
         {
             printf("%d X %d = %d\n",i,j,i*j);
-        }    
+        }
     }
-    
+
     return 0;
 }*/
 
-//wap to find factorial 
+// wap to find factorial
 /*int main(int argc, char const *argv[])
 {
     int num,c=1;
@@ -63,5 +63,72 @@
         c*=i;
     }
     printf("The factorial is %d",c);
+    return 0;
+}*/
+
+// wap to find the sum of all even numbers from a given range by the user
+/*int main(int argc, char const *argv[])
+{
+    int num1,num2,sum = 0;
+    printf("Enter starting number:-");
+    scanf("%d", &num);
+    printf("Enter ending number:-");
+    scanf("%d", &num);
+    for (int i = num1; i <= num2; i++)
+    {
+        if (i%2==0)
+        {
+            sum+=i;
+        }
+
+    }
+    printf("The sum of even numbers is %d",sum);
+    return 0;
+}*/
+// wap to find a fibonacci series by the number given by the user and also ask how many terms he wants
+void main(int argc, char const *argv[])
+{
+    int num1, n, num2;
+    printf("Enter number you want:-");
+    scanf("%d", &num1);
+    num2 = num1;
+    printf("Enter range you want:-");
+    scanf("%d", &n);
+    printf("%d\t", num1);
+    // printf("%d\t", num2);
+    for (int i = 1; i < n; i++)
+    {
+        int fibo = num1 + num2;
+        printf("%d\t", fibo);
+        num2 = num1;
+        num1 = fibo;
+    }
+}
+// wap to find cube of number upto a given integer
+/*int main(int argc, char const *argv[])
+{
+    int num;
+    printf("Enter number you want:-");
+    scanf("%d", &num);
+    for (int i = 1; i <= num; i++)
+    {
+        printf("%d\n",(i*i*i));
+    }
+
+    return 0;
+}*/
+// wap to sum of the number of a user provided numbers
+/*int main(int argc, char const *argv[])
+{
+    int num, n, sum = 0;
+    printf("Enter how many numbers you want:-");
+    scanf("%d", &num);
+    for (int i = 1; i < num+1; i++)
+    {
+        printf("Enter number %d :-",i);
+        scanf("%d", &n);
+            sum += n;
+    }
+    printf("The sum of even numbers is %d",sum);
     return 0;
 }*/
