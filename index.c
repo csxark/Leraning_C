72e41d4881f6ed143ba221f28d2c7bd9bd2ef4c5
csxark <arktandoncs@gmail.com>
2024-07-29 12:55:53 +0530

    Update index.c

--- index.c
+++ index.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    int *j= &n;
+    printf("The variable is:- %d\n",n);
+    printf("The variable location is:- %u\n",j);
+    printf("The variable is:- %d\n",*j); 
+    return 0;
+}*/
 
-
+/*void sum(int a , int b){
+    printf("The sum is %d\n",a+b);
+}
+void avg(int a,int b){
+    printf("The average is %d\n",(a+b)/2);
+}   
+int main(int argc, char const *argv[])
+{
+    int num1=49,num2=86;
+    int *ptr1=&num1,*ptr2=&num2;
+    sum(*ptr1,*ptr2);
+    avg(*ptr1,*ptr2); 
+    return 0;
+}*/
 
