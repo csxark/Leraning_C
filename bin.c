e1ec16e8f9c2c4e2cf3c5c6f4970464826dcb3d0
csxark <arktandoncs@gmail.com>
2024-11-06 01:08:36 +0530

    Update bin.c

--- bin.c
+++ bin.c
@@ -1,12 +1,7 @@
 #include<stdio.h>
 
 int main(int argc, char const *argv[])
-{   int n=10;
-int arr[32];
-for (int i = 1; i <=n; i++)
 {
-    /* code */
-}
-
+    printf("Hello World");
     return 0;
 }
