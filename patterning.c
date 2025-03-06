3d31f8a47dc6f40e988082776e1f4e92a7671d07
csxark <arktandoncs@gmail.com>
2025-03-07 01:38:05 +0530

    Update patterning.c

--- patterning.c
+++ patterning.c
@@ -1,21 +1,59 @@
 #include<stdio.h>
 
-int pattern(int n){
-    for (int i = 1; i < (n+1); i++)
+// int pattern(int n)
+//     for (int i = 1; i < (n+1); i++)
+//     {
+//         for (int j = 0; j <2*i-1; j++)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }  
+// }
+// int main(int argc, char const *argv[])
+// {
+//     int num;
+//     printf("Enter a number:- ");
+//     scanf("%d",&num);
+//     pattern(num);
+//     return 0;
+// }
+
+// int main(int argc, char const *argv[])
+// {
+//     int n =6;
+//     for (int i = 1; i <=n; i++)
+//     {
+//         for (int j = 1; j <=i; j++) 
+//         {
+//             printf("%d ",j*j);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+int main(int argc, char const *argv[])
+{
+    int n= 1;
+    for (int i = 1; i <= 6; i++)
     {
-        for (int j = 0; j <2*i-1; j++)
-        {
-            printf("*");
+        for (int j = 1; j <=i; j++)
+        {   
+            if (n%3==0)
+            {
+                n++; 
+            }else
+            {
+                printf("%d\t",n);
+                n++; 
+            }
+            
+            
         }
         printf("\n");
     }
     
-}
-int main(int argc, char const *argv[])
-{
-    int num;
-    printf("Enter a number:- ");
-    scanf("%d",&num);
-    pattern(num);
     return 0;
 }
